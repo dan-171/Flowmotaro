fa6ba835bb62b3d16b6ffb68a592a0a3466816f4
dan-171 <danh.cs0706@gmail.com>
2026-04-22 07:50:58 +0800

    create themes

---
+++ flowmotaro/lib/src/config/themes/light_theme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+ColorScheme colorScheme = ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 67, 67, 67),
+          primary: const Color.fromARGB(255, 67, 67, 67),
+          secondary: const Color.fromARGB(255, 135, 176, 53),
+          tertiary: const Color.fromARGB(255, 190, 218, 133),
+          primaryContainer: const Color.fromARGB(255, 190, 218, 133),
+        );
+
+ElevatedButtonThemeData elevatedButtonTheme = ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 67, 67, 67),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 2,
+            fixedSize: Size(250, 45),
+          ),
+        );
+
+TextTheme textTheme = TextTheme(
+      titleLarge: TextStyle(
+        fontWeight: FontWeight(800),
+      ),
+      titleMedium: TextStyle(
+        fontWeight: FontWeight(600),
+      )
+  );
