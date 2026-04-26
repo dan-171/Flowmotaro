fa67f04caeebaa7e8fc88cdacc0bf0d93d83ffc0
dan-171 <danh.cs0706@gmail.com>
2026-04-27 01:17:35 +0800

    update riverpod support

--- flowmotaro/lib/main.dart
+++ flowmotaro/lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flowmotaro/src/services/providers.dart';
 import 'package:flowmotaro/src/services/storage_services/isar_services.dart';
 import 'package:flowmotaro/src/ui/views/onboarding_view/onboarding_intro_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'src/config/themes/light_theme.dart';
 
@@ -12,7 +14,14 @@ void main() async{
     anonKey: "sb_publishable_fnque_xDyKR2xHvThteZQQ_kpA6RAf4",
   );
 
-  runApp(const Flowmotaro());
+  runApp(
+    ProviderScope(
+      overrides: [
+        isarProvider.overrideWithValue(IsarServices.instance),
+      ],
+      child: Flowmotaro()
+    ),
+  );
 }
 
 final supabase = Supabase.instance.client;
